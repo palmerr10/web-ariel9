ca7195bcf3abbd1863946b4f2f232ccbea0cdcaf
palmerr10 <69176713+palmerr10@users.noreply.github.com>
2020-08-06 09:55:32 -0300

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,1 +1,14 @@
 console.log("Test")
+
+const form = document.querySelector("#informacion")
+const email = document.querySelector("#email")
+
+form.addEventListener("submit", event => {
+	event.preventDefault()
+	if (email.value === "") {
+		console.log("Revisar info!")
+	} else { 
+		console.log ("Funciona")
+		form.submit();
+		}
+	})
